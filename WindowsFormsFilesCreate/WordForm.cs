32d6c216803c3c9d2a50c98b91fee3ed3c51e8d0
noskow.maximmaxim@yandex.ru <noskow.maximmaxim@yandex.ru>
2024-01-15 17:54:24 +0300

    Создание 2 формы для создания Word файлов

--- WindowsFormsFilesCreate/WordForm.cs
+++ WindowsFormsFilesCreate/WordForm.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsFilesCreate
 {
-    public partial class Form1 : Form
+    public partial class WordForm : Form
     {
-        public Form1()
+        public WordForm()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
